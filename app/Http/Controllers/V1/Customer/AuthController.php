0b86ea65b5b421d1be8ec03ade80cc30b51d3b45
amirhosein.hadii <amirhosein.hadi75@gmail.com>
2024-07-27 12:46:33 +0330

    Added jwt and login api

--- app/Http/Controllers/V1/Customer/AuthController.php
+++ app/Http/Controllers/V1/Customer/AuthController.php
@@ -3,12 +3,73 @@
 namespace App\Http\Controllers\V1\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function otpRequest()
+    public function otpRequest(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required|min:11|max:11',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->first()], 401);
+        }
+
+        $user = User::query()
+            ->firstOrCreate(
+                ['cellphone' => $request->mobile]
+            );
+
+        $user->otp_code = mt_rand(1111, 9999);
+        $user->save();
+
+//        dispatch(new SendSMSByPatternJob($cellphone, 'e72ao65rat8iy82', [['link' => 'lamtakam.com', 'title' => $user->otp_code]])); TODO
+
+        return response()->json(['message' => 'کد با موفقیت ارسال شد.'], 200);
+    }
+
+    public function otpVerification(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required|min:11|max:11',
+            'code' => 'required|string',
+        ]);
+
+        if ($validator->fails())
+            return response()->json(['error' => $validator->errors()], 401);
+
+        $user = User::query()->where('cellphone', $request->mobile)->first();
+
+        if (!is_object($user))
+            return response()->json(['error' => 'خطایی رخ داده است.'], 401);
+
+        if ($request->code <> $user->otp_code)
+            return response()->json(['error' => 'کد وارد شده اشتباه است.'], 401);
+
+        if (!$userToken = JWTAuth::claims(['exp' => Carbon::now()->addYear()->timestamp])->fromUser($user))
+            return response()->json(['error' => 'خطا در تولید کد ورود.'], 401);
+
+        $user->update(['otp_code' => null]);
+
+        $data = [
+            'access_token' => $userToken,
+            'token_type' => 'bearer'
+        ];
+
+        return response()->json(['message' => 'عملیات با موفقیت انجام شد.', 'data' => $data]);
+    }
+
+
+    public function getUser()
+    {
+        dd(Auth::user());
     }
 }
--- routes/v1/customer.php
+++ routes/v1/customer.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('auth')->group(function () {
-    Route::get('/otp-request', 'AuthController@otpRequest');
+    Route::post('/otp/request', 'AuthController@otpRequest');
+    Route::post('/otp/verify', 'AuthController@otpVerification');
+    Route::get('/user', 'AuthController@getUser')->middleware('auth:api');
 
 });
